895d73f8d44fed64b4c1884e0cb8103bac9e855d
Akshay Rathi <akshayrathi2000@gmail.com>
2022-02-23 11:26:54 +0530

    Add api calls

---
+++ api/session.js
@@ -0,0 +1,36 @@
+const BASE_URL = "http://localhost:3000/api/sessions";
+
+const createSession = async (name) => {
+  const response = await fetch(`${BASE_URL}/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify({
+      name
+    })
+  });
+
+  const session = response.json();
+  return session;
+}
+
+const playInSession = async (id, letter) => {
+  const response = await fetch(`${BASE_URL}/${id}/play`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8"
+    },
+    body: JSON.stringify({
+      letter
+    })
+  });
+
+  const session = response.json();
+  return session;
+}
+
+module.exports = {
+  createSession,
+  playInSession
+}
